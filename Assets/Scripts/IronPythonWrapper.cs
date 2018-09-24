7bc7fb5986a3ed6c561914b8a1e4b109ac222a13
Keith Matthews <keith@alice.caltech.edu>
2018-09-23 18:04:45 -0700

    Changed some variable names.

--- Assets/Scripts/IronPythonWrapper.cs
+++ Assets/Scripts/IronPythonWrapper.cs
@@ -16,16 +16,16 @@ public class IronPythonWrapper : MonoBehaviour {
 
 	public static void ScriptTest() {
 		// create the engine
-		var ScriptEngine = IronPython.Hosting.Python.CreateEngine();
+		var engine = IronPython.Hosting.Python.CreateEngine();
 
 		// and the scope (i.e. the Python namespace)
-		var ScriptScope = ScriptEngine.CreateScope();
+		var scope = engine.CreateScope();
 
 		// execute a string in the interpreter and grab the variable
 		string example = "output = 'hello world'";
 
-		ScriptEngine.CreateScriptSourceFromString(example).Execute(ScriptScope);
-		string came_from_script = ScriptScope.GetVariable<string>("output");
+		engine.CreateScriptSourceFromString(example).Execute(scope);
+		string came_from_script = scope.GetVariable<string>("output");
 
 		// Should be what we put into 'output' in the script.
 		Debug.Log(came_from_script);
